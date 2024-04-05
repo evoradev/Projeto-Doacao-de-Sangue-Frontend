9502ee82a66ccc8ae0e086d6b0d714d363b4f42c
Évora <122195163+Rafaelevora@users.noreply.github.com>
2024-04-04 21:01:15 -0300

    First Commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Formulario from './controllers/FormController.tsx'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Formulario />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
